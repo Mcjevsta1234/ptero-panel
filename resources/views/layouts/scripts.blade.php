f1adc0137d030511de74f65e8a2c5f95da6088d5
Mcjevsta1234 <jevanrichards2002@gmail.com>
2025-11-28 03:15:57 +0000

    Load React bundle in layouts/scripts to mount dedicated UI

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,1 +1,21 @@
-{{-- Just here as a binder for dynamically rendered content. --}}
+{{-- Inject compiled frontend assets (Webpack bundle) --}}
+@php
+	$assetDir = public_path('assets');
+	$bundle = null;
+	if (is_dir($assetDir)) {
+		$files = scandir($assetDir);
+		foreach ($files as $file) {
+			if (preg_match('/^bundle\..+\.js$/', $file)) {
+				$bundle = '/assets/' . $file;
+				break;
+			}
+		}
+	}
+@endphp
+
+@if ($bundle)
+	<script src="{{ $bundle }}" crossorigin="anonymous"></script>
+@else
+	{{-- Fallback for development builds or missing assets --}}
+	<script src="/assets/bundle.js" crossorigin="anonymous"></script>
+@endif
