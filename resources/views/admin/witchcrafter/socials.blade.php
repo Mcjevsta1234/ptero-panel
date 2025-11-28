7126bdc533772d6cd70ad5529e3f9c0bba58feb3
Mcjevsta1234 <jevanrichards2002@gmail.com>
2025-11-28 19:44:44 +0000

    h

--- resources/views/admin/witchcrafter/socials.blade.php
+++ resources/views/admin/witchcrafter/socials.blade.php
@@ -78,7 +78,6 @@
                     class="w-full px-4 py-3 bg-zinc-800/50 border border-zinc-700 rounded-xl text-white placeholder-zinc-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200"
                     placeholder="https://trials.witchyworlds.top" />
             </div>
-            </div>
         </div>
     </form>
 @endsection
