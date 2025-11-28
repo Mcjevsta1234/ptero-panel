fbb5975f79b3d9f0208a97675a195e1d53e699ce
Mcjevsta1234 <jevanrichards2002@gmail.com>
2025-11-28 04:02:26 +0000

    Only load React bundle on client pages, exclude admin area

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,21 +1,23 @@
-{{-- Inject compiled frontend assets (Webpack bundle) --}}
-@php
-	$assetDir = public_path('assets');
-	$bundle = null;
-	if (is_dir($assetDir)) {
-		$files = scandir($assetDir);
-		foreach ($files as $file) {
-			if (preg_match('/^bundle\..+\.js$/', $file)) {
-				$bundle = '/assets/' . $file;
-				break;
-			}
-		}
-	}
-@endphp
+{{-- Inject compiled frontend assets (Webpack bundle) for client-facing pages only --}}
+@if (!request()->is('admin') && !request()->is('admin/*'))
+    @php
+        $assetDir = public_path('assets');
+        $bundle = null;
+        if (is_dir($assetDir)) {
+            $files = scandir($assetDir);
+            foreach ($files as $file) {
+                if (preg_match('/^bundle\..+\.js$/', $file)) {
+                    $bundle = '/assets/' . $file;
+                    break;
+                }
+            }
+        }
+    @endphp
 
-@if ($bundle)
-	<script src="{{ $bundle }}" crossorigin="anonymous"></script>
-@else
-	{{-- Fallback for development builds or missing assets --}}
-	<script src="/assets/bundle.js" crossorigin="anonymous"></script>
+    @if ($bundle)
+        <script src="{{ $bundle }}" crossorigin="anonymous"></script>
+    @else
+        {{-- Fallback for development builds or missing assets --}}
+        <script src="/assets/bundle.js" crossorigin="anonymous"></script>
+    @endif
 @endif
