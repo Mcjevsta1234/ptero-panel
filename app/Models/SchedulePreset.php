410d90bc25a97f872eb56dc7d4b27081c329eac1
Mcjevsta1234 <jevanrichards2002@gmail.com>
2025-12-02 10:13:33 +0000

    d

--- app/Models/SchedulePreset.php
+++ app/Models/SchedulePreset.php
@@ -28,33 +28,3 @@ class SchedulePreset extends Model
         return $this->hasMany(SchedulePresetTask::class, 'preset_id');
     }
 }
-<?php
-
-namespace Pterodactyl\Models;
-
-use Illuminate\Database\Eloquent\Relations\HasMany;
-
-class SchedulePreset extends Model
-{
-    protected $table = 'schedule_presets';
-
-    protected $fillable = [
-        'name',
-        'description',
-        'cron_day_of_week',
-        'cron_month',
-        'cron_day_of_month',
-        'cron_hour',
-        'cron_minute',
-        'only_when_online',
-    ];
-
-    protected $casts = [
-        'only_when_online' => 'boolean',
-    ];
-
-    public function tasks(): HasMany
-    {
-        return $this->hasMany(SchedulePresetTask::class, 'preset_id');
-    }
-}
