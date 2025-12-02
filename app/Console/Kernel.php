af0dcd328e68578b3211a2ca0e33938c164a0432
Mcjevsta1234 <jevanrichards2002@gmail.com>
2025-12-02 13:30:20 +0000

    Change analytics collection from every 5 minutes to every minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,8 +35,8 @@ class Kernel extends ConsoleKernel
         $schedule->command(ProcessRunnableCommand::class)->everyMinute()->withoutOverlapping();
         $schedule->command(CleanServiceBackupFilesCommand::class)->daily();
 
-        // Collect server analytics every 5 minutes
-        $schedule->command('analytics:collect')->everyFiveMinutes();
+        // Collect server analytics every minute for real-time monitoring
+        $schedule->command('analytics:collect')->everyMinute();
 
         if (config('backups.prune_age')) {
             // Every 30 minutes, run the backup pruning command so that any abandoned backups can be deleted.
