5106b85b47e416f2f37a0bd687ff5ebd57f1d859
Mcjevsta1234 <jevanrichards2002@gmail.com>
2025-12-05 00:01:29 +0000

    Rename 'Modpack Installer' to 'Modpacks'

--- resources/lang/en/routes.php
+++ resources/lang/en/routes.php
@@ -25,7 +25,7 @@ return [
         'users' => 'Users',
         'settings' => 'Settings',
         'activity' => 'Activity',
-        'modpacks' => 'Modpack Installer',
+        'modpacks' => 'Modpacks',
         'cheatsheet' => 'Cheat Sheet',
     ],
 ];
