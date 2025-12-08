19c5e4b562c6605077023b0a4a8511633b0e9cb2
Mcjevsta1234 <jevanrichards2002@gmail.com>
2025-12-08 13:37:52 +0000

    Add debug comments to scripts.blade.php to diagnose bundle loading

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -4,11 +4,16 @@
         // Use webpack manifest for reliable asset loading
         $manifestPath = public_path('assets/manifest.json');
         $bundle = null;
+        $debugInfo = [];
         
         if (file_exists($manifestPath)) {
             $manifest = json_decode(file_get_contents($manifestPath), true);
+            $debugInfo['manifest_exists'] = true;
+            $debugInfo['manifest_keys'] = array_keys($manifest ?? []);
+            
             if (isset($manifest['bundle.js'])) {
                 $bundle = $manifest['bundle.js'];
+                $debugInfo['using_manifest'] = true;
             }
         }
         
@@ -20,12 +25,19 @@
                 foreach ($files as $file) {
                     if (preg_match('/^bundle\..+\.js$/', $file)) {
                         $bundle = '/assets/' . $file;
+                        $debugInfo['using_fallback'] = true;
+                        $debugInfo['fallback_file'] = $file;
                         break;
                     }
                 }
             }
         }
+        
+        $debugInfo['final_bundle'] = $bundle;
     @endphp
+
+    {{-- Debug comment visible in page source --}}
+    <!-- Bundle Debug: {{ json_encode($debugInfo) }} -->
 
     @if ($bundle)
         <script src="{{ $bundle }}" crossorigin="anonymous"></script>
